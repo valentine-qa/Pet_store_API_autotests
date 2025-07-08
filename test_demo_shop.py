e57c839fc561374dc14ea8e1787612f8f089c728
valentine-qa <valentine.borodich13@gmail.com>
2025-07-08 15:30:24 +0300

    Add tests

---
+++ test_demo_shop.py
@@ -0,0 +1,45 @@
+from time import sleep
+
+import requests
+from allure_commons._allure import step
+from selene import browser, have
+
+LOGIN = "example1200@example.com"
+PASSWORD = "123456"
+WEB_URL = "https://demowebshop.tricentis.com/"
+API_URL = "https://demowebshop.tricentis.com/"
+
+def test_login():
+    """Successful authorization to some demowebshop (UI)"""
+    with step("Open login page"):
+        browser.open("http://demowebshop.tricentis.com/login")
+
+    with step("Fill login form"):
+        browser.element("#Email").send_keys(LOGIN)
+        browser.element("#Password").send_keys(PASSWORD).press_enter()
+
+    with step("Verify successful authorization"):
+        browser.element(".account").should(have.text(LOGIN))
+
+def test_login_with_API():
+    """Successful authorization to some demowebshop (UI)"""
+
+    with step("Login with API"):
+        request = requests.request("POST",
+            url='https://demowebshop.tricentis.com/login',
+            data={"Email": LOGIN, "Password": PASSWORD, "RememberMe": False},
+            allow_redirects=False)
+
+    print(request.cookies)
+    print(request.status_code)
+    with step("Get cookie from API"):
+      cookie = request.cookies.get("NOPCOMMERCE.AUTH")
+
+    with step("Set cookie from API"):
+        browser.open(WEB_URL)
+        browser.driver.add_cookie({"name": "NOPCOMMERCE.AUTH", "value": cookie})
+        browser.open(WEB_URL)
+
+    with step("Verify successful authorization"):
+        browser.element(".account").should(have.text(LOGIN))
+    sleep(5)
--- test_reqres.py
+++ test_reqres.py
@@ -23,3 +23,23 @@ def test():
   assert response.status_code == 201
   with open("post_users.json") as file:
     jsonschema.validate(body, schema=json.loads(file.read()))
+
+
+
+def test_job_name_returns_in_response():
+  job = "leader"
+  name = "morpheus"
+  response = requests.request("POST", url, headers=headers, data=payload)
+  body = response.json()
+  assert body["name"] == name
+  assert body["job"] == job
+  print(body)
+  print(response.json())
+
+def test_single_user():
+  url = "https://reqres.in/api/users"
+  response = requests.request("GET", url, params={"page": 2, "per_page": 4}, headers=headers)
+  ids = [element["id"] for element in response.json()["data"]]
+  set_ids = set(ids)
+  set_ids = set_ids[1:]
+  print()
